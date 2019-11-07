a3abcf62f643e70794c9fe2e954dd2c6785a51c1
Hod <hod.gohasi@hotmail.com>
2019-11-07 21:57:54 +0200

    first commit

--- app/src/main/java/com/example/androidhw/MainActivity.java
+++ app/src/main/java/com/example/androidhw/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.androidhw;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button doneBtn=(Button)findViewById(R.id.doneBtn);
+        doneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                show me a message with TODA:) in the same time that i click on done
+//                Toast.makeText(getApplicationContext(),"TODA :)",Toast.LENGTH_LONG).show();
+                
+//                show in message the sequance that write on editText line
+                EditText textField=(EditText)findViewById(R.id.textField);
+                Toast.makeText(getApplicationContext(),textField.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
